10a38c28a6829c69e67403c692ce05c094aefeaa
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-10-27 15:26:22 +0300

    server | docs : add documentation for deal module server | fix : eslint configure file

--- server/src/deals/dto/create-deal.dto/create-deal.dto.ts
+++ server/src/deals/dto/create-deal.dto/create-deal.dto.ts
@@ -25,15 +25,3 @@ export class CreateDealDto {
   @IsString()
   readonly sold: string;
 }
-
-// {
-//     id: 1,
-//     img: 'someImageUrl',
-//     objName: 'someObjName',
-//     currency: 'Dhs',
-//     objPrice: 6500000,
-//     ticketPrice: 60000,
-//     yield: '9.25%',
-//     daysLeft: 150,
-//     sold: '75%'
-// }
